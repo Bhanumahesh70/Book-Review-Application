632bfee9e392809706a5b149563f0a08fe66603c
Bhanumahesh <bhanumahesh70@gmail.com>
2024-04-25 13:27:59 -0500

    User model created

---
+++ Book_Review_App/models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+// Create Schema for User
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: [5, 'Username must be at least 5 characters long'],
+        maxlength: [30, 'Username cannot exceed 30 characters']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [8, 'Password must be at least 8 characters long'],
+        maxlength: [100, 'Password cannot exceed 100 characters']
+    }
+}, {
+    timestamps: true // adds createdAt and updatedAt timestamps automatically
+});
+
+// Pre-save hook to hash password before saving if it's modified
+userSchema.pre('save', function(next) {
+    let user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+// Compile model from schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
