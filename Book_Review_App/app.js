f2707a9ac5268df370698dda054d4283d43426ba
Bhanumahesh <bhanumahesh70@gmail.com>
2024-04-26 14:32:28 -0500

    bookrouter  added

--- Book_Review_App/app.js
+++ Book_Review_App/app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 require('./config/passport')(passport); // pass passport for configuration
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Book_Review_App/routes/books.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require('../controllers/bookController');
+
+// Middleware to ensure users are authenticated before allowing access to certain routes
+const isAuthenticated = require('../middlewares/isAuthenticated');
+
+// GET request for the list of all book entries.
+router.get('/', bookController.book_list);
+
+// GET request for creating a book. This must come before routes that display Book (uses id)
+router.get('/create', isAuthenticated, bookController.book_create_get);
+
+// POST request for creating Book
+router.post('/create', isAuthenticated, bookController.book_create_post);
+
+// GET request to delete Book
+router.get('/:id/delete', isAuthenticated, bookController.book_delete_get);
+
+// POST request to delete Book
+router.post('/:id/delete', isAuthenticated, bookController.book_delete_post);
+
+// GET request to update Book
+router.get('/:id/update', isAuthenticated, bookController.book_update_get);
+
+// POST request to update Book
+router.post('/:id/update', isAuthenticated, bookController.book_update_post);
+
+// GET request for one Book's detail
+router.get('/:id', bookController.book_detail);
+
+module.exports = router;
