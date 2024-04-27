90b30ccdb1d70aa7f5c5ec012752ee1795192b2d
Bhanumahesh <bhanumahesh70@gmail.com>
2024-04-27 14:34:13 -0500

    userController added

--- Book_Review_App/routes/users.js
+++ Book_Review_App/routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET request for registering a new user
+router.get('/register', userController.user_create_get);
+
+// POST request for registering a new user
+router.post('/register', userController.user_create_post);
+
+// GET request for user login
+router.get('/login', userController.user_login_get);
+
+// POST request for user login
+router.post('/login', 
+    passport.authenticate('local', {
+        successRedirect: '/', // Redirect to the homepage on successful login
+        failureRedirect: '/users/login', // Redirect back to the login page if there's an error
+        failureFlash: true // Optional: Use flash messages to report login errors
+    })
+);
+
+// GET request for logging out
+router.get('/logout', userController.user_logout_get);
 
 module.exports = router;
